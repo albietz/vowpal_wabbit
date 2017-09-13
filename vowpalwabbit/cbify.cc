d365d1a6a2627fc8ae2c676844a674cae13902be
Alberto Bietti <alberto@bietti.me>
2017-09-13 10:32:41 -0400

    cbify: print_entropy option

--- vowpalwabbit/cbify.cc
+++ vowpalwabbit/cbify.cc
@@ -52,6 +52,7 @@ struct cbify
   cbify_adf_data adf_data;
   float loss0;
   float loss1;
+  bool print_entropy;
 };
 
 vector<float> vw_scorer::Score_Actions(example& ctx)
@@ -138,6 +139,16 @@ void copy_example_to_adf(cbify& data, example& ec)
   }
 }
 
+double compute_entropy(const v_array<action_score>& a_s)
+{ double ent = 0.0;
+  for (size_t idx = 0; idx < a_s.size(); ++idx)
+  { const double p = a_s[idx].score;
+    if (p > 0 && p < 1)
+      ent -= p * log(p);
+  }
+  return ent;
+}
+
 template <bool is_learn, bool use_cs>
 void predict_or_learn(cbify& data, base_learner& base, example& ec)
 {
@@ -156,6 +167,9 @@ void predict_or_learn(cbify& data, base_learner& base, example& ec)
   //Call the cb_explore algorithm. It returns a vector of probabilities for each action
   base.predict(ec);
   //data.probs = ec.pred.scalars;
+
+  if (data.print_entropy)
+    cout << compute_entropy(ec.pred.a_s) << endl;
 
   uint32_t action = data.mwt_explorer->Choose_Action(*data.generic_explorer, StringUtils::to_string(data.example_counter++), ec);
 
@@ -202,6 +216,8 @@ void predict_or_learn_adf(cbify& data, base_learner& base, example& ec)
   base.predict(*data.adf_data.empty_example);
   // get output scores
   auto& out_ec = data.adf_data.ecs[0];
+  if (data.print_entropy)
+    cout << compute_entropy(out_ec.pred.a_s) << endl;
   uint32_t idx = data.mwt_explorer->Choose_Action(
                    *data.generic_explorer,
                    StringUtils::to_string(data.example_counter++), out_ec) - 1;
@@ -254,7 +270,8 @@ base_learner* cbify_setup(vw& all)
   new_options(all, "CBIFY options")
   ("cbify_cs", "consume cost-sensitive classification examples instead of multiclass")
   ("loss0", po::value<float>(), "loss for correct label")
-  ("loss1", po::value<float>(), "loss for incorrect label");
+  ("loss1", po::value<float>(), "loss for incorrect label")
+  ("print_entropy", "print entropy of exploration distribution to stdout");
   add_options(all);
 
   po::variables_map& vm = all.vm;
@@ -264,6 +281,7 @@ base_learner* cbify_setup(vw& all)
   data.use_adf = count(all.args.begin(), all.args.end(),"--cb_explore_adf") > 0;
   data.loss0 = vm.count("loss0") ? vm["loss0"].as<float>() : 0.f;
   data.loss1 = vm.count("loss1") ? vm["loss1"].as<float>() : 1.f;
+  data.print_entropy = vm.count("print_entropy") > 0;
   data.recorder = new vw_recorder();
   data.mwt_explorer = new MwtExplorer<example>("vw",*data.recorder);
   data.scorer = new vw_scorer();
