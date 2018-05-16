00906e76e7cea8e7e8bc49fda84f0a3ed3730f98
Alberto Bietti <alberto@bietti.me>
2018-05-16 13:27:36 +0200

    fix

--- vowpalwabbit/cb_explore_adf.cc
+++ vowpalwabbit/cb_explore_adf.cc
@@ -262,7 +262,7 @@ void get_disagree_weights(std::vector<float>& weights, cb_explore_adf& data, bas
 // TODO: same as cs_active.cc, move to shared place
 float binary_search(float fhat, float delta, float sens, float tol=1e-6)
 {
-  const float maxw = min(fabs(fhat) / sens, FLT_MAX);
+  const float maxw = min(fhat / sens, FLT_MAX);
 
   if (maxw * fhat * fhat <= delta)
     return maxw;
@@ -329,13 +329,13 @@ void get_cost_ranges(std::vector<float> &min_costs,
       min_costs[a] = cmin;
     else
     {
-      w = binary_search(ec->pred.scalar - cmin - 1, delta, sens);
+      w = binary_search(ec->pred.scalar - cmin + 1, delta, sens);
       min_costs[a] = max(ec->pred.scalar - sens * w, cmin);
+      // cout << ec->pred.scalar << " " << sens << " " << w << " " << ec->pred.scalar - sens * w
+      //      << " | ";
       if (min_costs[a] > cmax)
         min_costs[a] = cmax;
     }
-      cout << ec->pred.scalar << " " << sens << " " << w << " " << min_costs[a]
-           << " | ";
 
     if (!min_only)
     {
@@ -356,7 +356,7 @@ void get_cost_ranges(std::vector<float> &min_costs,
       // cout << sens << " " << w << " " << max_costs[a] << ", ";
     }
   }
-  cout << endl;
+  // cout << endl;
 
   // reset cb example data
   for (size_t i = 0; i < examples.size(); ++i)
@@ -573,13 +573,13 @@ void predict_or_learn_regcb(cb_explore_adf& data, base_learner& base, v_array<ex
     get_cost_ranges(data.min_costs, data.max_costs, delta, data, base, examples,
                     /*min_only=*/data.regcbopt);
 
-    for (size_t i = 0; i < num_actions; ++i)
+    /*for (size_t i = 0; i < num_actions; ++i)
     {
       cout << "(" << data.min_costs[preds[i].action] << ", "
         << preds[i].score << ", " << (data.regcbopt ? 0. : data.max_costs[preds[i].action])
         << ") ";
     }
-    cout << endl;
+    cout << endl;*/
 
     if (data.regcbopt) // optimistic variant
     {
